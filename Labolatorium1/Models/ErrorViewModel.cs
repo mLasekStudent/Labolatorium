57663cb8b22f3d34c5b035176aad22efde4178af
LasekM <poltok00@gmail.com>
2023-10-15 18:02:01 +0200

    Add project files.

---
+++ Labolatorium1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Labolatorium1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
